280b1fd5a7891d992489c4317aa45cf168435f3b
Mauro Midolo <mauro.midolo@hotmail.com>
2020-03-21 21:12:08 +0100

    Added fake test

--- TineiMatewai/src/test/java/com/tousinho/client/ClientTest.java
+++ TineiMatewai/src/test/java/com/tousinho/client/ClientTest.java
@@ -1,7 +1,12 @@
 package com.tousinho.client;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ClientTest {
-    
+    @Test
+    public void polpettaIsPolpetta() {
+        assertEquals("Polpetta","Polpetta");
+    }
 }
