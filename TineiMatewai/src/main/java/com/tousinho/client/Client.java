d0cc0f7d5beeefba09d363ccb1548c46be93541b
Mauro Midolo <mauro.midolo@hotmail.com>
2020-03-22 14:20:43 +0100

    Resolved problem of import

--- TineiMatewai/src/main/java/com/tousinho/client/Client.java
+++ TineiMatewai/src/main/java/com/tousinho/client/Client.java
@@ -1,6 +1,10 @@
 package com.tousinho.client;
 
-import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.PinState;
 
 public class Client {
 
--- TineiMatewai/src/test/java/com/tousinho/client/ClientTest.java
+++ TineiMatewai/src/test/java/com/tousinho/client/ClientTest.java
@@ -2,7 +2,7 @@ package com.tousinho.client;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class ClientTest {
     @Test
