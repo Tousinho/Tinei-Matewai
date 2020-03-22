226e505031750803be8e846a65a5eff6df856787
Mauro Midolo <mauro.midolo@hotmail.com>
2020-03-22 12:40:38 +0100

    Added PumpController class

---
+++ TineiMatewai/src/main/java/com/tousinho/client/controller/PumpController.java
@@ -0,0 +1,38 @@
+package com.tousinho.client.controller;
+
+import com.pi4j.io.gpio.*;
+
+public class PumpController {
+
+    private static PumpController pumpController;
+    private final GpioPinDigitalOutput pin;
+
+    public static PumpController getInstance() {
+        if (pumpController == null) {
+            pumpController = new PumpController();
+        }
+        return pumpController;
+    }
+
+    private PumpController() {
+        final Pin gpioPin = RaspiPin.GPIO_01;
+        final GpioController gpio = GpioFactory.getInstance();
+        pin = gpio.provisionDigitalOutputPin(gpioPin, "Pump", PinState.LOW);
+    }
+
+    public PinState getPinState() {
+        return pin.getState();
+    }
+
+    public void setPinStatusHigh(){
+        setStatus(PinState.HIGH);
+    }
+
+    public void setPinStatusLow(){
+        setStatus(PinState.LOW);
+    }
+
+    private void setStatus(PinState pinState) {
+        pin.setState(pinState);
+    }
+}
