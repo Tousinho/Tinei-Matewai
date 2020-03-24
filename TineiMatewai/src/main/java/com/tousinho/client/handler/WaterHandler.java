b2d36032423eb13003274db8087c2a2b15a8ad27
Mauro Midolo <mauro.midolo@hotmail.com>
2020-03-24 22:03:48 +0100

    Added log4j 2 in WaterHandler

--- TineiMatewai/src/main/java/com/tousinho/client/handler/WaterHandler.java
+++ TineiMatewai/src/main/java/com/tousinho/client/handler/WaterHandler.java
@@ -2,10 +2,13 @@ package com.tousinho.client.handler;
 
 import com.tousinho.client.controller.HumidityController;
 import com.tousinho.client.controller.PumpController;
-
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class WaterHandler implements Runnable {
+
+    private static final Logger logger = LogManager.getLogger(WaterHandler.class.getName());
+
     private HumidityController humidityController;
     private PumpController pumpController;
 
@@ -17,7 +20,9 @@ public class WaterHandler implements Runnable {
 
     @Override
     public synchronized void run() {
+        logger.info("Checking if it is time to water");
         if (humidityController.isTimeToWater()) {
+            logger.info("it's time to water!");
             pumpController.putWater();
         }
     }
