34d68c04417511cffa90020a0c105a093b2dcc4b
Mauro Midolo <mauro.midolo@hotmail.com>
2020-03-28 14:55:09 +0100

    added curl command

--- bash-script/run-last-snapshot.sh
+++ bash-script/run-last-snapshot.sh
@@ -5,8 +5,9 @@ JAR_ABSOLUTE_PATH="$SCRIPTPATH/Tinei-Matewai.jar"
 
 echo "Cleaning space..."
 rm -f "$JAR_ABSOLUTE_PATH"
-echo "Downloading Tinei-Matewai.jar latest verion..."
+echo "Downloading Tinei-Matewai.jar snapshot latest verion..."
 wget -q -O "$JAR_ABSOLUTE_PATH" --content-disposition "https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=com.github.tousinho&a=Tinei-Matewai&v=LATEST&e=jar&c=jar-with-dependencies"
+#curl -Ls -o "$JAR_ABSOLUTE_PATH" "https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=com.github.tousinho&a=Tinei-Matewai&v=LATEST&e=jar&c=jar-with-dependencies"
 
 echo "Starting application..."
 java -jar "$JAR_ABSOLUTE_PATH"
