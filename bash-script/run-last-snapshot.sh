28f7c54ded1db5adfbdd3f20e8c6287e95e67fda
Mauro Midolo <mauro.midolo@hotmail.com>
2020-03-28 15:35:35 +0100

    Added service

--- bash-script/run-last-snapshot.sh
+++ bash-script/run-last-snapshot.sh
@@ -9,5 +9,11 @@ echo "Downloading Tinei-Matewai.jar snapshot latest verion..."
 wget -q -O "$JAR_ABSOLUTE_PATH" --content-disposition "https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=com.github.tousinho&a=Tinei-Matewai&v=LATEST&e=jar&c=jar-with-dependencies"
 #curl -Ls -o "$JAR_ABSOLUTE_PATH" "https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=com.github.tousinho&a=Tinei-Matewai&v=LATEST&e=jar&c=jar-with-dependencies"
 
+echo "Congifure Service..."
+#sudo cp tinei.service /etc/systemd/system/tinei.service
+
 echo "Starting application..."
-java -jar "$JAR_ABSOLUTE_PATH"
+
+#sudo systemctl start tinei
+#sudo systemctl enable tinei
+#java -jar "$JAR_ABSOLUTE_PATH"
